b5ccbfc65e0cdad5c94fdc745bc0369e66ae73ca
Professor Guilherme <guilhermealonsoribeiro>
2026-05-06 14:41:31 -0300

    corrigindo o database

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-const mysql = require('mysql2');
-
-// criar a conexão com o banco de dados
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'projeto_api_tarefas'
-});
-
-// conecta ao Banco de dados
-connection.connect((err) => {
-    if (err) {
-        console.error('Erro ao conectar ao banco de dados: ' + err);
-        return;
-    }
-    console.log('Conectado ao banco de dados com sucesso!');
-=======
 const mysql = require("mysql2");
 
 // cria conexão com banco
@@ -34,7 +15,6 @@ connection.connect((err) => {
         return;
     }
     console.log("Conectado ao MySQL!");
->>>>>>> aula-2904
 });
 
 module.exports = connection;
